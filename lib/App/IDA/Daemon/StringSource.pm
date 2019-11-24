b2cc0e6caa212155047b848684c3bfa22c44de0e
Declan Malone <idablack@users.sourceforge.net>
2019-11-24 18:15:21 +0000

    Bulk up the documentation.

--- lib/App/IDA/Daemon/StringSource.pm
+++ lib/App/IDA/Daemon/StringSource.pm
@@ -55,6 +55,7 @@ __END__
  use App::IDA::Daemon::StringSource;
  use Mojo::IOLoop;
 
+ # Stream a string 3 characters at a time
  my $stream = App::IDA::Daemon::StringSource
   ->new("Mojo::IOLoop", "My String", 3);
 
@@ -66,14 +67,66 @@ __END__
 
 =head1 DESCRIPTION
 
-Mojo::IOLoop::Stream objects only handle "streaming" file handles, so
-it doesn't work (fully) with regular files. Nor does it with the kind
-of file handle created as follows:
+This module implements a "treat a string as a stream" idea in a way
+that should be compatible with Mojo::IOLoop::Stream.
+
+Mojo::IOLoop::Stream objects only handle "streaming" file handles
+(fifos, sockets, pipes, etc.), so it doesn't work (fully) with regular
+files. Nor does it work with the kind of file handle created as
+follows:
 
  # read from a string via a perl file handle
  my $string = "I'm not a file, but to many I look like one";
  open my $fh, "<", \$string;
 
-This module implements the same kind of "treat a string as a stream"
-idea in a way that should be compatible with Mojo::IOLoop::Stream.
+The natural use for this class is in test scripts where it's more
+conventient to stream from string literals, without needing to set up
+test files.
+
+=head1 EVENTS EMITTED
+
+This class derives from C<Mojo::EventEmitter>. Objects emit these
+events just like a Mojo::IOLoop::Stream object would:
+
+=over
+
+=item read
+
+More data from the string becomes available (once per event loop tick).
+
+=item close
+
+Emitted after string has been consumed (eof) or close() method called.
+
+=back
+
+=head1 METHODS
+
+The following method calls are available:
+
+=over
+
+=item new($event_loop, $string, $chunk_size)
+
+Hooks into the event loop ('C<Mojo::IOLoop>') to set up repeated
+streaming of C<$chunk_size> bytes from C<$string>.
+
+C<$chunk_size> can be undefined, in which case it defaults to the
+length of the string.
+
+=item start
+
+Set state of the object to "running". You also need to have the event
+loop running before any events will be emitted.
+
+=item stop
+
+Stops the object running. Useful in a C<once(read ...)>
+callback to make sure you don't miss any read chunks.
+
+=item close
+
+Manually "close" the stream.
+
+=back
 
