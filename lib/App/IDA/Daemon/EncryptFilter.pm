9e959714edf6df1a169e4e3f087c1d64cd7821f5
Declan Malone <idablack@users.sourceforge.net>
2019-11-25 13:08:23 +0000

    Encryptor side.

--- lib/App/IDA/Daemon/EncryptFilter.pm
+++ lib/App/IDA/Daemon/EncryptFilter.pm
@@ -15,6 +15,8 @@ sub config {
 }
 
 use CryptX;
+use Crypt::Mode::CBC;
+use Crypt::PRNG::RC4 qw(random_bytes);
 
 sub new {
     my ($class, $source, $read, $close, $key) = @_;
@@ -23,16 +25,47 @@ sub new {
 	return undef;
     }
 
-    # For now, just implement NullFilter (input -> output unchanged)
+    # create a random initialisation vector
+    my $iv = random_bytes(16);	# AES iv size
     my $self = bless {
 	source => $source,
 	read   => $read,
 	close  => $close,
+	iv     => $iv,
     }, $class;
 
-    $source->on($read => sub { $self->emit(read => $_[1]) });
-    return $self unless defined($close);
-    $source->on($close => sub { $self->emit("close") });
+    # Set up the encryptor in CBC mode
+    my $enc = Crypt::Mode::CBC->new($settings{cipher});
+    unless (defined $enc) {
+	warn "Failed to create $settings{cipher} encryptor in CBC mode\n";
+	return undef;
+    }
+    $enc->start_encrypt($key, $iv);
+
+    # The IV will be passed unencrypted at the start of the data
+    # stream. We can't emit anything right now, though (since there
+    # can't be any subscribers), so we'll have to arrange to pass it
+    # during a regular emit chain.
+    $source->on($read => 
+		sub {
+		    my $indata = $_[1];
+		    my $outdata = $enc->add($indata);
+		    if (defined $iv) {
+			$outdata = $iv . $outdata;
+			$iv = undef;
+		    }
+		    $self->emit(read => $outdata);
+		});
+    unless (defined $close) {
+	# On eof, we need to finish the encryption
+	warn "EncryptFilter must subscribe to a close event\n";
+	return undef;
+    };
+    $source->on($close =>
+		sub {
+		    $self->emit(read => $enc->finish);
+		    $self->emit("close")
+		});
     $self;
 }
 
